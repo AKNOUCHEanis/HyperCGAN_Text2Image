424d9e84f83c0549e1ea596d77f0104b9f21d314
AKNOUCHEanis <ga_aknouche@esi.dz>
2021-12-28 16:00:44 +0100

    Hypernetwork TGS BODY HEAD

--- Main.py
+++ Main.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
-
+from Model import HyperNetwork_TGS_Head, HyperNetwork_TGS_Body
+import torch
 
 if __name__=="__main__":
     
-    print("main")
+    dim_z = 256
+    c_in =  512
+    c_out = 512
+    kh, kw = 3,3
     
+    dim_latent1 = 256
+    dim_latent2 = 300
     
+    hypnet_body = HyperNetwork_TGS_Body(dim_z, dim_latent1, dim_latent2)
+
+    hypnet_head = HyperNetwork_TGS_Head(dim_h = dim_latent2 , c_in=c_in, c_out=c_out, kh=kh , kw=kw)
     
-    print("fin")
+    z = torch.rand(dim_z)
     
-    jjjjjjjjjjjjjj
-    
-    jjjjjjjjjjjj
-    jjj
+    h = hypnet_body.forward(z)
+    w = hypnet_head.forward(h)
+    print(w.shape)
